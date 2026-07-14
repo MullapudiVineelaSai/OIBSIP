996cc25ca5f05ffce8bfbb0ad4d6c793826f7a9b
MullapudiVineelaSai <mullapudivinni123@gmail.com>
2026-07-14 20:16:25 +0530

    Add documentation for Task5_client.py
    Added documentation for the chat application, including project description, features, running instructions, and technologies used.

---
+++ Python-Task5-ChatApplication/Task5_client.py
@@ -0,0 +1,35 @@
+# Task 5: Chat Application - Advanced Tier
+### Oasis Infobyte Internship Program
+
+## Project Description:
+Advanced Real-time Chat Application with GUI using Python Tkinter and Socket Programming.
+Supports multiple clients, timestamps, and live server log.
+
+## Features:
+- GUI Interface: Tkinter based chat window
+- Multi-client Support: Multiple users can chat at once
+- Timestamps: Every message shows time
+- Online Users: Server shows who connected
+- Broadcast Messages: Message goes to all connected clients
+- Threading: Send and Receive simultaneously
+
+## How to Run:
+1. Install Python 3.x. No extra pip install needed
+2. Open Terminal
+3. PS C:\Users\mulla\OneDrive\Desktop\Oasis-Internship\Python-Task5-ChatApplication>
+Step 1: Run Server
+python Task5_server.py
+
+Step 2: Run Client
+python Task5_client.py
+Enter your name and start chatting
+
+Step 3: Open multiple client windows to test
+
+## Technologies Used:
+- Python 3.x
+- tkinter, socket, threading, datetime
+
+## Server Details:
+- Host: 127.0.0.1
+- Port: 12345
